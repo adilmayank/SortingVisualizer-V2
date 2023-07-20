e9d68f1d2c7d222db1937fb48f2f79867a02a5f5
adilmayank <adilmayank5894@gmail.com>
2023-07-20 18:27:13 +0530

    Removed unnecessary console.log

--- src/Toast.jsx
+++ src/Toast.jsx
@@ -2,18 +2,17 @@ import { useEffect } from 'react'
 
 const Toast = ({ notification, setNotification }) => {
   useEffect(() => {
-    console.log(notification)
     return () => {
       setTimeout(() => {
         setNotification(null)
       }, 5000)
     }
   }, [])
-
-
   return (
-    <div className="toast absolute left-0 top-6 flex justify-center items-center w-72 h-10 z-10 bg-green-400 rounded-md ring-1 ring-green-900">
-      <div className="m-1 p-1 w-full flex justify-center text-white font-semibold uppercase">{notification}</div>
+    <div className="toast absolute left-0 top-6 flex justify-center items-center w-72 h-10 z-10 bg-slate-600/70 rounded-md ring-1 ring-green-900">
+      <div className="m-1 p-1 w-full flex justify-center text-white font-semibold uppercase">
+        {notification}
+      </div>
     </div>
   )
 }
